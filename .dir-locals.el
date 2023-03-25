9d7a1da78b3c1dc1dfcc5a827871060bce3996cd
Mark A. Hershberger <mah@nichework.com>
2023-03-25 15:19:22 -0400

    Initial cruft

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;; This is here to enable emacs users to use tab-completion and other features provided by Red Hat's
+;; ansible language server.  See https://emacs-lsp.github.io/lsp-mode/page/lsp-ansible/
+;;
+;; Users of VS Code can use the Red Hat's VSCode extension:
+;; https://www.ansible.com/blog/deep-dive-on-ansible-vscode-extension
+((yaml-mode . ((eval . (progn
+                         (ansible)
+                         (lsp))
+                     ))))
